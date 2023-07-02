9d56ae548485f34bbbc273de5f5928bcc912832f
raf61 <rafamauricio0712@gmail.com>
2023-07-02 18:39:51 -0300

    adjusted vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: path.resolve(__dirname, "./dist/")
+  base: ''
   // original Vue configuration
   // resolve: {
   //   alias: {
